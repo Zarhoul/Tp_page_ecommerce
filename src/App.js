501ef5a670899addaaedae3a7654e1d55dad52ab
Zarhoul <zarhoul21@gmail.com>
2023-01-16 15:35:18 +0100

    Edit content product.js & App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Product from './components/product';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Product />
     </div>
   );
 }
--- src/components/product.js
+++ src/components/product.js
@@ -1,11 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Product() {
     const [products,SetProducts] = useState([]);
+    useEffect(() => {axios.get("../data/products.json").then(res => SetProducts(res.data))},[]);
 
-  return (
-    <div>product</div>
-  )
+    return (
+        <> 
+        <div>
+            {products.map((elem,index) => 
+                <div key={index}>
+                    <img src="" alt="..."/>
+                    <div>
+                        <h3>{elem.title}</h3>
+                        <h5>{elem.currentPrice}</h5>
+                        <button>Add to cart</button>
+                    </div>
+                </div>
+            )}
+        </div>
+        </>
+    )
 }
 
 export default Product
