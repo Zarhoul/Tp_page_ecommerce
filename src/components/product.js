25c527b7877e89627e79c47ababad49b024d4e8a
Zarhoul <zarhoul21@gmail.com>
2023-01-16 15:18:40 +0100

    Edit header.js & product.js | Add data file products.json

---
+++ src/components/product.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+function Product() {
+    const [products,SetProducts] = useState([]);
+
+  return (
+    <div>product</div>
+  )
+}
+
+export default Product
